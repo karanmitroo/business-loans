cf8eee1e9b45bdbaa7b3baddf55c58ff9f598055
Karan Mitroo <karan1.mitroo@gmail.com>
2018-12-05 10:24:32 +0530

    DB setup

--- business_loans/settings.py
+++ business_loans/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'business_loans.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'business_loans',
     }
 }
 
