d37dbd8e1c4cd18e2862639be029250cbf39bb08
Karan Mitroo <karan1.mitroo@gmail.com>
2018-12-07 14:28:17 +0530

    linting changes

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import JSONField
+from django.db import models
+
 # Create your models here.
 
 class UserData(models.Model):
--- users/views.py
+++ users/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
+from rest_framework.authentication import (BasicAuthentication,
+                                           SessionAuthentication)
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from users.models import UserData
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
-
+    """ Hackable method to bypass csrf check while getting a post request """
     def enforce_csrf(self, request):
         return
 
@@ -19,6 +22,7 @@ class Register(APIView):
 
     @classmethod
     def post(cls, request):
+        """ Will be called when a user tries to register to the platform """
         username = request.data.get('username')
         password = request.data.get('password')
 
@@ -27,19 +31,18 @@ class Register(APIView):
             # If the user already exists, then ask them to login rather registering.
             return Response("User Already exists")
 
-        else:
-            # Else register and signin the user.
-            user_obj = User.objects.create(username=username)
-            user_obj.set_password(password)
-            user_obj.save()
+        # Else register and signin the user.
+        user_obj = User.objects.create(username=username)
+        user_obj.set_password(password)
+        user_obj.save()
 
-            # Authenticate the user and log them in.
-            authenticated_user = authenticate(request, username=username, password=password)
-            login(request, authenticated_user)
+        # Authenticate the user and log them in.
+        authenticated_user = authenticate(request, username=username, password=password)
+        login(request, authenticated_user)
 
-            # Also create the other models required further.
-            cls.create_userdata(authenticated_user)
-            return Response("Thanks for logging in")
+        # Also create the other models required further.
+        cls.create_userdata(authenticated_user)
+        return Response("Thanks for logging in")
 
 
     @classmethod
@@ -55,6 +58,7 @@ class Login(APIView):
 
     @classmethod
     def post(cls, request):
+        """ Will be called when a user tries to login to the platform """
         username = request.data.get('username')
         password = request.data.get('password')
 
@@ -65,5 +69,19 @@ class Login(APIView):
             login(request, user_obj)
             return Response("Logged In")
 
-        else:
-            return Response("Sorry, Incorrect username/ password entered.")
+        return Response("Sorry, Incorrect username/ password entered.")
+
+
+
+class Eligibility(APIView):
+    """ To check if a user is eligibile or not for the loan """
+
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+
+    @classmethod
+    def get(cls, request):
+        """
+        Will be called to check eligibility for a company before getting them registering
+        to the platform.
+        """
+        return Response("Done")
